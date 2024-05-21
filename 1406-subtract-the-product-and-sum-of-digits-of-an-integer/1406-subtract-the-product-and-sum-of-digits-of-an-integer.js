25f08c3ed5466619ac0c464c52c4f7a1ae6b0a6a
Ytqwerty <106899395+Ytqwerty@users.noreply.github.com>
2024-05-21 10:27:23 +0500

    Time: 55 ms (38.57%), Space: 48.8 MB (51.66%) - LeetHub

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.js
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -5,12 +5,11 @@
 var subtractProductAndSum = function(n) {
     let sum = 0;
     let product = 1
-    let result = 0
     let array = n.toString().split('').map(Number);
     for (let i = 0;i<array.length;i++) {
         sum+=array[i]
         product*=array[i]
     }
-    result = product - sum
-    return result
+    return product - sum
+    
 };
