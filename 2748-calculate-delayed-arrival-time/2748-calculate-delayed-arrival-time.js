13de5b933b9a635f9833ba95f333dff2f3de8fa9
Ytqwerty <106899395+Ytqwerty@users.noreply.github.com>
2024-05-29 14:27:34 +0500

    Time: 53 ms (66.9%), Space: 49.4 MB (7.04%) - LeetHub

---
+++ 2748-calculate-delayed-arrival-time/2748-calculate-delayed-arrival-time.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} arrivalTime
+ * @param {number} delayedTime
+ * @return {number}
+ */
+var findDelayedArrivalTime = function(arrivalTime, delayedTime) {
+    let time = arrivalTime + delayedTime
+    return time >= 24 ? time - 24 : time
+};
