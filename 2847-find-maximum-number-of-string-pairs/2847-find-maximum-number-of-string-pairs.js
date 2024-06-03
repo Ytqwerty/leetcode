7655b8181649c180cb4f90462d105c94c113d5c7
Ytqwerty <106899395+Ytqwerty@users.noreply.github.com>
2024-06-03 14:10:28 +0500

    Time: 86 ms (22.07%), Space: 55.5 MB (14.32%) - LeetHub

---
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var maximumNumberOfStringPairs = function(words) {
+   let count = 0
+    for(let i=0;i<words.length;i++){
+        for(let j=i+1;j<words.length;j++){
+            if(words[i].split("").reverse().join("") === words[j]){
+                count++
+            }
+        }
+    }
+    return count
+};
