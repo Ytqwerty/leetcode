193194e400d664900d412c1203a5df52743d2122
Ytqwerty <106899395+Ytqwerty@users.noreply.github.com>
2024-09-05 13:17:18 +0500

    Time: 71 ms (66.03%), Space: 54.9 MB (50.07%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function(names, heights) {
+   let array = []
+   let result = []
+   for (let i = 0;i<heights.length;i++) {
+    array.push([names[i],heights[i]])
+   }
+   array.sort((a,b) => b[1] - a[1])
+   for (const key of array) {
+    result.push(key[0])
+   }
+   return result
+};
